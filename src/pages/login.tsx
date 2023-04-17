09d651b507a2fbd5d8a476b16a05ebf7694e9626
Philip Kuo <pkgamma@gmail.com>
2023-04-17 01:31:55 -0500

    changed login button look

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -10,7 +10,7 @@ function Login({ providers }: LoginProps) {
       {Object.values(providers).map((provider: any) => (
         <div key={provider.name}>
           <button
-            className="bg-[#18D860] text-white p-4 rounded-full"
+            className="text-white bg-[#18D860] hover:bg-[#18D860]/90 focus:ring-4 focus:outline-none focus:ring-[#18D860]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2"
             onClick={() => signIn(provider.id, { callbackUrl: "/" })}
           >
             Sign in with {provider.name}
