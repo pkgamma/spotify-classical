77b62b7ad9d0d0538d04366acfc3a4383d91bd2e
Philip Kuo <pkgamma@gmail.com>
2023-04-17 20:25:07 -0500

    removed lucide-react and some other unused dependencies

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,11 +4,10 @@ import { getProviders, getSession, signIn } from "next-auth/react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { buttonVariants } from "@/components/ui/button";
-import { Icons } from "@/components/ui/icons";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
-import { Command } from "lucide-react";
+import { HomeIcon } from "@heroicons/react/outline";
 
 export const metadata: Metadata = {
   title: "Authentication",
@@ -22,32 +21,12 @@ interface LoginProps {
 function Login({ providers }: LoginProps) {
   return (
     <>
-      {/* <div className="md:hidden">
-        <Image
-          src="/examples/authentication-light.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/authentication-dark.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="hidden dark:block"
-        />
-      </div> */}
-      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Link
-          href="/"
-          className={cn(
-            buttonVariants({ variant: "ghost", size: "sm" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
-        >
+      <div className="md:hidden">
+        <Button onClick={() => signIn("spotify", { callbackUrl: "/" })}>
           Login
-        </Link>
+        </Button>
+      </div>
+      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div
             className="absolute inset-0 bg-cover"
@@ -57,7 +36,7 @@ function Login({ providers }: LoginProps) {
             }}
           />
           <div className="relative z-20 flex items-center text-lg font-medium">
-            <Command className="mr-2 h-6 w-6" /> Acme Inc
+            Spotify Classics
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
@@ -73,14 +52,12 @@ function Login({ providers }: LoginProps) {
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
-              </h1>
+              <h1 className="text-2xl font-semibold">Create an account</h1>
               <p className="text-sm text-muted-foreground">
                 Enter your email below to create your account
               </p>
             </div>
-            {/* user auth form starts */}
+            {/* user auth form starts ============================== */}
             <div className="grid gap-6">
               <div className="relative">
                 <div className="absolute inset-0 flex items-center">
@@ -100,12 +77,12 @@ function Login({ providers }: LoginProps) {
                   variant="outline"
                   type="button"
                 >
-                  <Icons.moon className="mr-2 h-4 w-4" />
+                  <HomeIcon className="mr-2 h-4 w-4" />
                   {provider.name}
                 </Button>
               ))}
             </div>
-            {/* ends */}
+            {/* ends ============================================ */}
             <p className="px-8 text-center text-sm text-muted-foreground">
               By clicking continue, you agree to our{" "}
               <Link
