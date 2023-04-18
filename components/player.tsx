6c28de814c8e79696f42b1842c1f865d56f4c212
Philip Kuo <pkgamma@gmail.com>
2023-04-18 00:14:10 -0500

    currentely playing track info done

--- components/player.tsx
+++ components/player.tsx
@@ -1,18 +1,68 @@
+import { currentTrackState, isPlayingState } from "@/atoms/songAtom";
 import useSongInfo from "@/hooks/useSongInfo";
 import useSpotify from "@/hooks/useSpotify";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { useEffect } from "react";
+import { useRecoilState } from "recoil";
 
 function Player() {
   const spotifyApi = useSpotify();
   const { data: session, status } = useSession();
+  const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
 
   const songInfo = useSongInfo();
 
+  const handlePlayPause = () => {
+    spotifyApi.getMyCurrentPlaybackState().then((response) => {
+      if (response.body) {
+        if (response.body.is_playing) {
+          spotifyApi.pause();
+          setIsPlaying(false);
+        } else {
+          spotifyApi.play();
+          setIsPlaying(true);
+        }
+      }
+    });
+  };
+
+  const fetchCurrentSong = () => {
+    if (!songInfo) {
+      spotifyApi.getMyCurrentPlayingTrack().then((response) => {
+        if (response.body) {
+          setCurrentTrackId(response.body.item.id);
+        }
+      });
+      spotifyApi.getMyCurrentPlaybackState().then((response) => {
+        if (response.body) {
+          setIsPlaying(true);
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken() && !currentTrackId) {
+      fetchCurrentSong();
+    }
+  }, [currentTrackState, spotifyApi, session]);
+
   return (
     <div>
-      <h1>Player</h1>
-      <Image src={songInfo?.album?.images?.[0].url} width={200} height={200} />
+      <Image
+        src={songInfo?.album?.images?.[0].url}
+        width={100}
+        height={100}
+        alt=""
+      />
+
+      {isPlaying ? (
+        <button onClick={handlePlayPause}>Pause</button>
+      ) : (
+        <button onClick={handlePlayPause}>Play</button>
+      )}
     </div>
   );
 }
